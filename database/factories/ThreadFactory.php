f685a156723b30b4c35be82d8cb552605600f8b0
Nikolay Zhuravlev <gaaved@gmail.com>
2022-01-05 14:26:02 +0200

    Initial commit

---
+++ database/factories/ThreadFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Thread;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+
+class ThreadFactory extends Factory
+{
+    protected $model = Thread::class;
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+
+    public function definition()
+    {
+        return [
+
+
+            'user_id' => User::factory(),
+
+            'title' =>$this->faker->sentence,
+            'body' =>$this->faker->paragraph
+        ];
+    }
+}
